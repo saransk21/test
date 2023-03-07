0c595ffe3296bf8036f8cfc29e982401eb7c6824
unknown <saran.kumar@tringapps.com>
2023-03-07 11:02:40 +0530

    big

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const test1 = () =>{
     console.log("big feature")
 }
 test1()
+const test2 = () =>{
+    console.log("big feature")
+}
+test1()
+const test3 = () =>{
+    console.log("big feature")
+}
+test1()
 
 
 
